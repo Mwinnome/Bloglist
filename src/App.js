255e556ea8430a7149b2b49d21d4fc97051d39c0
Mwinnome <101268966+Mwinnome@users.noreply.github.com>
2022-03-23 04:15:13 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import React, { useState } from 'react'
+
+function App() {
+const [title, setTitle]= useState("")
+const [body, setBody]= useState("")
+const [author, setAuthor] =useState("Ama")
+
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  const blog ={title, body, author};
+  console.log(blog);
+}
+
+  return (
+    <div className='create'>
+    <h2>Add a new Blog</h2>
+
+    <form onSubmit={handleSubmit}>
+    <label>BLog Title:</label>
+    <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
+    
+    <label>Blog Body:</label>
+    <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+
+    <label>Blog Author</label>
+    <select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+    <option value="Ama">Ama</option>
+    <option value="Ama">Kofi</option>
+    <option value="Ama">Adwoa</option>
+    </select>
+     
+    <button>Add Blog</button>
+
+    </form>
+    </div>
+  )
+}
+
+export default App
